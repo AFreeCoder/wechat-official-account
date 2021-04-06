c587bfb3e9ea3a31a55ad26070a0132dbc4fe407
wanghaijie01 <wanghaijie01@baidu.com>
2021-04-07 01:32:46 +0800

    add logger middleware

--- httpserver/server.go
+++ httpserver/server.go
@@ -2,17 +2,43 @@
  * @Description: Do not edit
  * @Date: 2021-03-20 02:05:29
  * @LastEditors: wanghaijie01
- * @LastEditTime: 2021-03-28 02:13:42
+ * @LastEditTime: 2021-04-05 19:57:18
  */
 
 package httpserver
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/gin-gonic/gin"
+	"github.com/wechat-offical-account/logger"
+)
+
+type ServerConfig struct {
+	Base struct {
+		Name    string `json:"name"`
+		Listen  string `json:"listen"`
+		RunMode string `json:"run_mode"`
+	} `json:"base"`
+	WeChat struct {
+		Token string `json:"token"`
+	} `json:"wechat"`
+}
+
+func loadConfig() (*ServerConfig, error) {
+	configFile := "./conf/httpserver.toml"
+	config := &ServerConfig{}
+	if _, err := toml.DecodeFile(configFile, config); err != nil {
+		panic(err)
+	}
+	return config, nil
+}
 
 func Start() {
+	serverConf, _ := loadConfig()
 	r := gin.Default()
 
-	RouterSetup(r)
+	r.Use(logger.Logger())
 
-	r.Run(":80")
+	RouterSetup(r)
+	r.Run(serverConf.Base.Listen)
 }
