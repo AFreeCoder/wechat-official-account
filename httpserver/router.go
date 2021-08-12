75503c6bc4496181284468c53b85df9c354322e9
AFreeCoder <afreecoder@163.com>
2021-08-12 20:04:44 +0800

    add reverseproxy

--- httpserver/router.go
+++ httpserver/router.go
@@ -2,18 +2,25 @@
  * @Description: Do not edit
  * @Date: 2021-03-20 01:40:57
  * @LastEditors: wanghaijie01
- * @LastEditTime: 2021-03-21 00:49:40
+ * @LastEditTime: 2021-08-12 19:44:40
  */
 
 package httpserver
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/wechat-official-account/httpserver/controller"
+	"github.com/wechat-official-account/httpserver/controller/proxy"
 )
 
 // RouterSetup 路由设置
 func RouterSetup(r *gin.Engine) {
 	r.GET("/wechat", controller.DefaultController.Get)
+	r.GET("/index.html", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.Any("/proxy", proxy.Proxy)
 	r.POST("/wechat", controller.DefaultController.Post)
 }
