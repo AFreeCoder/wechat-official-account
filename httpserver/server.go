8d4f472e3403ecb3e996b4c8a4f103b0504375c2
wanghaijie01 <wanghaijie01@baidu.com>
2021-09-13 00:12:12 +0800

    support https

--- httpserver/server.go
+++ httpserver/server.go
@@ -2,12 +2,14 @@
  * @Description: Do not edit
  * @Date: 2021-03-20 02:05:29
  * @LastEditors: wanghaijie01
- * @LastEditTime: 2021-08-12 15:19:41
+ * @LastEditTime: 2021-09-13 00:11:03
  */
 
 package httpserver
 
 import (
+	"fmt"
+
 	"github.com/BurntSushi/toml"
 	"github.com/gin-gonic/gin"
 	"github.com/wechat-official-account/logger"
@@ -15,9 +17,11 @@ import (
 
 // ServerConfig server 配置
 type ServerConfig struct {
-	Name    string `json:"name"`
-	Listen  string `json:"listen"`
-	RunMode string `json:"run_mode"`
+	Name           string `toml:"name"`
+	Listen         string `toml:"listen"`
+	PemPath        string `toml:"pem_path"`
+	PrivateKeyPath string `toml:"private_key_path"`
+	RunMode        string `toml:"run_mode"`
 }
 
 func loadConfig() (*ServerConfig, error) {
@@ -34,8 +38,7 @@ func Start() {
 	serverConf, _ := loadConfig()
 	r := gin.Default()
 	r.Use(logger.Logger())
-	r.Static("/static/", "./static/")
-	r.LoadHTMLFiles("./static/index.html")
 	RouterSetup(r)
-	r.Run(serverConf.Listen)
+	fmt.Println(serverConf)
+	r.RunTLS(serverConf.Listen, serverConf.PemPath, serverConf.PrivateKeyPath)
 }
